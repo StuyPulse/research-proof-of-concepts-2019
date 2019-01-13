541ee46e70b2d557c1f0229878bf6947351ce858
Kevin16777216 <kcai00@stuy.edu>
2019-01-13 02:52:42 -0500

    finish NEO Drivetrain + start on FollowerCommand.java

--- src/main/java/frc/robot/commands/Follow.java
+++ src/main/java/frc/robot/commands/Follow.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -9,14 +9,28 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import java.io.File;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.followers.EncoderFollower;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+public class Follow extends Command {
+  EncoderFollower leftFollower; 
+	EncoderFollower rightFollower; 
+	
+	File leftCSV; 
+	File rightCSV; 
+	
+	Trajectory leftTraj; 
+	Trajectory rightTraj;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+	Notifier profileProcessor; 
+	double dt; 
+  public Follow() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+    requires(Robot.Drivetrain);
   }
 
   // Called just before this Command runs the first time
