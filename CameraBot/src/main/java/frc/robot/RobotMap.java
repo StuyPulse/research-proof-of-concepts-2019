6f70faf78fc668fbe9b917a2af0b04ed220a306d
vs2961 <33765960+vs2961@users.noreply.github.com>
2019-01-16 18:49:15 -0500

    Line sensor poc (#2)
    * Add widget for shuffleboard. I'm pretty sure I did it wrong, and it doesn't work. Problems: I didn't know what the properties were, and what kind of map you were supposed to make. Resources:
    CameraStream class, ShuffleBoard class, BuiltInWidgets enum (part of suffleboard class). I didn't understand how the properties portion of the file was supposed to work
    
    * Add Line Sensor POC
    
    * Edit code in order to properly use usb camera. Will need to find all the proper ports and paths. Not sure if the code works b/c I had to use a ComplexWidget, which doesn't have the getEntry() method that SimpleWidget has, and as a result, I can't can't store it within NetworkTables. Will need to do testing to see if the code works
    
    * Fix class used for Line Sensor
    
    * make line sensor class
    
    * Add devlog, -Jin, Shirley and Declan
    
    * add driveTrain to LineSensorBot

---
+++ CameraBot/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+public final class RobotMap {
+    public final static int CAMERA_STREAM = -1;
+    public final static int VIDEO_SOURCE = -1;
+    public final static int CAMERA_PATH = -1;
+}
